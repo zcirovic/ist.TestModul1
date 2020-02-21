3da4d0ca45d20c36e8335cae413be5bc14601d02
zoran <zoran.cirovic@gmail.com>
2020-02-21 19:57:17 +0100

    v-1.0.0

---
+++ sharedData.js
@@ -0,0 +1,12 @@
+const sadGradovi = require("cities");
+exports.grad = (zip)=>{
+    return sadGradovi.zip_lookup(zip)
+};
+
+exports.poruke1 = [
+    "Dobro jutro!",
+    "Dobar dan!",
+    "Dobro veče!",
+    "Laku noć"
+];
+
